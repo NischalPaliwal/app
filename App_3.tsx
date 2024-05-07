3aad9fd64f0b07b435706a81badf7a4107c4d62e
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-05-07 23:34:26 +0530

    edit

--- App_3.tsx
+++ App_3.tsx
@@ -4,6 +4,13 @@ import React from 'react'
 // Form Validation
 import * as Yup from 'yup'
 
+const PasswordSchema = Yup.object().shape({
+    passwordLength: Yup.number()
+    .min(4,'Should be of min. 4 characters')
+    .max(16,'Should be of max. 16 characters')
+    .required('Length is required')
+})
+
 export default function App_3() {
   return (
     <View>
