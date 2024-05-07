d16275e571929d83eafe97e361aab3c8495b2939
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-05-07 23:26:38 +0530

    edit

---
+++ App_3.tsx
@@ -0,0 +1,15 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+// Form Validation
+import * as Yup from 'yup'
+
+export default function App_3() {
+  return (
+    <View>
+      <Text>App_3</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
