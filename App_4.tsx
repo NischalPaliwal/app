d3f4733bbd96655cb74e3a48efbb11560ebf790c
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-05-16 21:19:33 +0530

    edit

---
+++ App_4.tsx
@@ -0,0 +1,12 @@
+import { NavigationContainer } from "@react-navigation/native"
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs"
+import List1 from "./components/List1"
+import List2 from "./components/List2"
+
+const Tabs = createBottomTabNavigator()
+
+function tabs() {
+    return()
+}
+
+export default tabs();
