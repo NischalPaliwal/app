87a0aa935c7cad4d0f018f030d05a8b57cceac6a
Nischal Paliwal <156404171+NischalPaliwal@users.noreply.github.com>
2024-05-20 23:25:29 +0530

    icon

--- App_4.tsx
+++ App_4.tsx
@@ -3,7 +3,9 @@ import {
     Pressable,
     StyleSheet,
   Text,
-  View
+  View,
+  TouchableOpacity,
+  Image
 } from 'react-native';
 
 import { NavigationContainer } from '@react-navigation/native';
@@ -12,6 +14,8 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import List1 from './components/List1';
 import List2 from './components/List2';
 import List3 from './components/List3';
+import { shadow } from 'react-native-paper';
+import App_1 from './App_1'
 
 const tabs = createBottomTabNavigator()
 
@@ -26,10 +30,113 @@ function App(): JSX.Element {
        }}>
         <tabs.Screen 
         name = 'Home' 
-        component={List1}
+        component={App_1}
+        options={{
+          tabBarIcon: ({focused}) => (
+          <View style = {{
+          alignItems: 'center',
+          justifyContent: 'center',
+          top: 10
+          }}>
+            <Image
+            source = {require('./assets/icons/home.png')}
+            resizeMode = 'contain'
+            style = {{
+            width: 30,
+            height: 30,
+            tintColor: '#FFFFFF',
+            }}
+            />
+            <Text style = {{
+              color: '#FFFFFF',
+              fontSize: 14,
+              fontWeight: '800',
+            }}>HOME</Text>
+          </View>
+          ),
+        }}
         />
-        <tabs.Screen name = 'Profile' component={List2}/>
-        <tabs.Screen name = 'Settings' component={List3}/>
+        <tabs.Screen 
+        name = 'Profile' 
+        component={List2}
+        options={{
+          tabBarIcon: ({focused}) => (
+          <View style = {{
+          alignItems: 'center',
+          justifyContent: 'center',
+          top: 10
+          }}>
+            <Image
+            source = {require('./assets/icons/user.png')}
+            resizeMode = 'contain'
+            style = {{
+            width: 30,
+            height: 30,
+            tintColor: '#FFFFFF',
+            }}
+            />
+            <Text style = {{
+              color: '#FFFFFF',
+              fontSize: 14,
+              fontWeight: '800',
+            }}>PROFILE</Text>
+          </View>
+          ),
+        }}/>
+        <tabs.Screen 
+        name = 'Settings' 
+        component={List3}
+        options={{
+          tabBarIcon: ({focused}) => (
+          <View style = {{
+          alignItems: 'center',
+          justifyContent: 'center',
+          top: 10
+          }}>
+            <Image
+            source = {require('./assets/icons/setting.png')}
+            resizeMode = 'contain'
+            style = {{
+            width: 30,
+            height: 30,
+            tintColor: '#FFFFFF',
+            }}
+            />
+            <Text style = {{
+              color: '#FFFFFF',
+              fontSize: 14,
+              fontWeight: '800',
+            }}>SETTINGS</Text>
+          </View>
+          ),
+        }}/>
+        <tabs.Screen 
+        name = 'Help' 
+        component={List3}
+        options={{
+          tabBarIcon: ({focused}) => (
+          <View style = {{
+          alignItems: 'center',
+          justifyContent: 'center',
+          top: 10
+          }}>
+            <Image
+            source = {require('./assets/icons/help.png')}
+            resizeMode = 'contain'
+            style = {{
+            width: 30,
+            height: 30,
+            tintColor: '#FFFFFF',
+            }}
+            />
+            <Text style = {{
+              color: '#FFFFFF',
+              fontSize: 14,
+              fontWeight: '800',
+            }}>HELP</Text>
+          </View>
+          ),
+        }}/>
        </tabs.Navigator>
     </NavigationContainer>
   );
@@ -44,8 +151,19 @@ const styles = StyleSheet.create({
         left: 20,
         right: 20,
         elevation: 0,
-        backgroundColor: '#ffffff',
+        backgroundColor: '#1d5191',
         borderRadius: 15,
-        height: 90
+        height: 90,
+        ...shadow
+    },
+    shadow: {
+    shadowColor: '#7F5DF0',
+    shadowOffset: {
+      width: 0,
+      height: 10
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.5,
+    elevation: 5
     }
 })
